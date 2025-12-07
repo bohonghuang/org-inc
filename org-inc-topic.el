793ef276911ef46909083059c9bc033d3ec1125a
Bohong Huang <bohonghuang@qq.com>
2025-12-07 14:35:03 +0800

    Adapt to the lastest version of Org-srs

--- org-inc-topic.el
+++ org-inc-topic.el
@@ -74,40 +74,44 @@
     (org-srs-item-narrow)
     (org-srs-item-add-hook-once
      'org-srs-item-after-confirm-hook
-     (lambda ()
-       (when (org-srs-reviewing-p)
-         (when (<= entry-beginning (point) entry-end)
-           (when (and (or (not (boundp 'org-inc-continue)) (symbol-value 'org-inc-continue))
-                      (not (boundp 'org-srs-reviewing-p)))
-             (condition-case nil
-                 (if (and (org-srs-item-cloze-collect entry-beginning entry-end) (y-or-n-p "Transform this topic to cloze deletions?"))
-                     (org-inc-transform 'cloze)
-                   (let ((args (org-srs-review-rate t)))
-                     (org-with-wide-buffer
-                      (goto-char entry-beginning)
-                      (org-inc-topic-scale-priority (alist-get 'priority-scale args)))))
-               (quit (apply #'org-srs-item-review type args)))))))
+     (org-srs-review-item-hook
+      (lambda ()
+        (when (org-srs-reviewing-p)
+          (when (<= entry-beginning (point) entry-end)
+            (when (and (or (not (boundp 'org-inc-continue)) (symbol-value 'org-inc-continue))
+                       (not (boundp 'org-srs-reviewing-p)))
+              (condition-case nil
+                  (if (and (org-srs-item-cloze-collect entry-beginning entry-end) (y-or-n-p "Transform this topic to cloze deletions?"))
+                      (org-inc-transform 'cloze)
+                    (let ((args (org-srs-review-rate t)))
+                      (org-with-wide-buffer
+                       (goto-char entry-beginning)
+                       (org-inc-topic-scale-priority (alist-get 'priority-scale args)))))
+                (quit (apply #'org-srs-item-review type args))))))))
      90)
     (apply (org-srs-item-confirm) type args)))
 
-(cl-defmethod org-srs-item-review :before (type &rest args)
-  (when type
-    (let ((entry-beginning (copy-marker (org-srs-entry-beginning-position)))
-          (entry-end (copy-marker (org-srs-entry-end-position)))
-          (point nil))
-      (org-srs-item-add-hook-once
-       'org-srs-item-after-confirm-hook
-       (lambda () (setf point (point-marker)))
-       0)
-      (org-srs-item-add-hook-once
-       'org-srs-item-after-confirm-hook
-       (lambda ()
-         (when (org-srs-reviewing-p)
-           (unless (<= entry-beginning point entry-end)
-             (goto-char entry-beginning)
-             (org-srs-item-confirm-cleanup)
-             (apply #'org-srs-item-review type args))))
-       95))))
+(defun org-inc-topic-setup-before-item-review (type &rest args)
+  (let ((entry-beginning (copy-marker (org-srs-entry-beginning-position)))
+        (entry-end (copy-marker (org-srs-entry-end-position)))
+        (point nil))
+    (org-srs-item-add-hook-once
+     'org-srs-item-after-confirm-hook
+     (org-srs-review-item-hook
+      (lambda () (setf point (point-marker))))
+     0)
+    (org-srs-item-add-hook-once
+     'org-srs-item-after-confirm-hook
+     (org-srs-review-item-hook
+      (lambda ()
+        (when (org-srs-reviewing-p)
+          (unless (<= entry-beginning point entry-end)
+            (goto-char entry-beginning)
+            (org-srs-item-confirm-cleanup)
+            (apply #'org-srs-item-review type args)))))
+     95)))
+
+(add-hook 'org-srs-item-before-review-hook #'org-inc-topic-setup-before-item-review)
 
 (defun org-srs-query-predicate-topic ()
   (lambda () (eq (org-inc-item-args-type) 'topic)))
